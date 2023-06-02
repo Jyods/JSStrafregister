afee0114d57c31ddbbb96388ef34c88ad289080a
Jyodisen <jyodisen_sprenger@sluz.ch>
2023-06-02 12:12:46 +0200

    test

--- src/router/index.js
+++ src/router/index.js
@@ -107,7 +107,7 @@ const router = createRouter({
   ]
 })
 
-router.beforeEach(async (to, from, next) => {
+/*router.beforeEach(async (to, from, next) => {
   const isAuthenticated = await auth() // Hier können Sie Ihre eigene Authentifizierungsfunktion implementieren
   if (to.name !== 'Login' && !isAuthenticated) {
     //TODO: Add check if the User is still active
@@ -116,6 +116,6 @@ router.beforeEach(async (to, from, next) => {
     next()
   }
 })
-
+*/
 
 export default router
