857d24d4a925cac0a29ae4424d632379d56840fa
Jyods <jyodi.sprenger@hotmail.ch>
2023-08-01 01:09:59 +0200

    fixed backend route

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,4 +1,4 @@
-const backend = 'http://jsstrafregisterbackend.test/api';
+const backend = 'https://home-5013253951.app-ionos.space/public/api';
 
 export async function getFiles() {
     const response = await fetch(`${backend}/files`,{
